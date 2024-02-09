743ed54b6c8a2c2e10caeeb0e25a38203ae784d0
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-02-09 15:51:24 +0100

    creazione cartelle e import header e main

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <script>
-import AppHeader from './components/AppHeader.vue';
+import AppHeader from './components/Header/AppHeader.vue'
+import AppSearch from './components/main/AppSearch.vue'
+import CharactersList from './components/main/CharactersList.vue'
+
 
 export default {
-  components: {
-  
+  components:{
+    AppHeader,
+    AppSearch,
+    CharactersList
   }
+  
 }
 </script>
 
 <template>
- 
+  <AppHeader/>
+  <main>
+    <AppSearch/>
+    <CharactersList/>
+  </main>
+
 </template>
 
 <style lang="scss">
-@use './style/general.scss' as *;
+
 </style>
---
+++ src/components/Header/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="py-3">
+        <div class="container">
+        <h1 class="text-center">yu-gi-oh API</h1>
+        </div>
+    </header>
+    
+</template>
+
+<script>
+    export default {
+        name:"AppHeader",
+        data () {
+
+            return{}
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
