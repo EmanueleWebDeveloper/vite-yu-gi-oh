ec053f46c287f345a39ebc78b351f3fea6882185
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-02-09 14:59:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: {
+  
+  }
+}
+</script>
+
+<template>
+ 
+</template>
+
+<style lang="scss">
+@use './style/general.scss' as *;
+</style>
